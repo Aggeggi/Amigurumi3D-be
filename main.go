32d9546d6fad5b40508d5cd20a2b2c114f99009e
linda <me@lindapaolieri.com>
2024-04-03 23:23:16 +0200

    init project

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	_ "github.com/Aggeggi/Amigurumi3D-be/docs"
+	"github.com/Aggeggi/Amigurumi3D-be/routes"
+
+	"github.com/gin-gonic/gin"
+	"github.com/swaggo/files"
+	"github.com/swaggo/gin-swagger"
+)
+
+// gin-swagger middleware
+// swagger embed files
+//	@title			Amigurumi 3D
+//	@version		1.0
+//	@description	This is a server for the amigurumi 3D app
+
+//	@host		localhost:8080
+//	@BasePath	/api/v1
+
+func main() {
+	r := gin.Default()
+	v1 := r.Group("/api/v1")
+	{
+
+		v1.GET("/ping", routes.Ping)
+	}
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
---
+++ routes/healthcheck.go
@@ -0,0 +1,22 @@
+package routes
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// @BasePath /api/v1
+
+// PingExample godoc
+// @Summary ping example
+// @Schemes
+// @Description do ping
+// @Tags example
+// @Accept json
+// @Produce json
+// @Success 200 {string} Helloworld
+// @Router /example/helloworld [get]
+func Ping(g *gin.Context)  {
+	g.JSON(http.StatusOK,"helloworld")
+}
